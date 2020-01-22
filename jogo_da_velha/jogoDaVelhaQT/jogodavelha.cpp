3af1d773aacad0f8ec44bc00bddb4a92f732c038
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-01-22 15:31:21 -0400

    adicionado método para iniciar o jogo

--- jogo_da_velha/jogoDaVelhaQT/jogodavelha.cpp
+++ jogo_da_velha/jogoDaVelhaQT/jogodavelha.cpp
@@ -6,6 +6,8 @@ JogoDaVelha::JogoDaVelha(QWidget *parent)
     , ui(new Ui::JogoDaVelha)
 {
     ui->setupUi(this);
+
+    setStatus(false);
 }
 
 JogoDaVelha::~JogoDaVelha()
@@ -13,3 +15,84 @@ JogoDaVelha::~JogoDaVelha()
     delete ui;
 }
 
+
+void JogoDaVelha::setStatus(bool status)
+{
+    ui->btn_field1->setEnabled(status);
+    ui->btn_field2->setEnabled(status);
+    ui->btn_field3->setEnabled(status);
+    ui->btn_field4->setEnabled(status);
+    ui->btn_field5->setEnabled(status);
+    ui->btn_field6->setEnabled(status);
+    ui->btn_field7->setEnabled(status);
+    ui->btn_field8->setEnabled(status);
+    ui->btn_field9->setEnabled(status);
+}
+
+void JogoDaVelha::clearFields()
+{
+    fieldsSelected.clear();
+    fieldsSelected.resize(9);
+    fieldsSelected.fill(0);
+    ui->btn_field1->setText("");
+    ui->btn_field2->setText("");
+    ui->btn_field3->setText("");
+    ui->btn_field4->setText("");
+    ui->btn_field5->setText("");
+    ui->btn_field6->setText("");
+    ui->btn_field7->setText("");
+    ui->btn_field8->setText("");
+    ui->btn_field9->setText("");
+
+}
+
+void JogoDaVelha::on_btn_start_clicked()
+{
+    clearFields();
+    setStatus(true);
+}
+
+void JogoDaVelha::on_btn_field1_clicked()
+{
+    ui->btn_field1->setText("X");
+}
+
+void JogoDaVelha::on_btn_field2_clicked()
+{
+    ui->btn_field2->setText("X");
+}
+
+void JogoDaVelha::on_btn_field3_clicked()
+{
+    ui->btn_field3->setText("X");
+}
+
+void JogoDaVelha::on_btn_field4_clicked()
+{
+    ui->btn_field4->setText("X");
+}
+
+void JogoDaVelha::on_btn_field5_clicked()
+{
+    ui->btn_field5->setText("X");
+}
+
+void JogoDaVelha::on_btn_field6_clicked()
+{
+    ui->btn_field6->setText("X");
+}
+
+void JogoDaVelha::on_btn_field7_clicked()
+{
+    ui->btn_field7->setText("X");
+}
+
+void JogoDaVelha::on_btn_field8_clicked()
+{
+    ui->btn_field8->setText("X");
+}
+
+void JogoDaVelha::on_btn_field9_clicked()
+{
+    ui->btn_field9->setText("X");
+}
--- jogo_da_velha/jogoDaVelhaQT/jogodavelha.h
+++ jogo_da_velha/jogoDaVelhaQT/jogodavelha.h
@@ -2,6 +2,7 @@
 #define JOGODAVELHA_H
 
 #include <QMainWindow>
+#include <QVector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class JogoDaVelha; }
@@ -15,7 +16,34 @@ public:
     JogoDaVelha(QWidget *parent = nullptr);
     ~JogoDaVelha();
 
+    void setStatus(bool status);
+    void clearFields();
+
+private slots:
+    void on_btn_start_clicked();
+
+    void on_btn_field1_clicked();
+
+    void on_btn_field2_clicked();
+
+    void on_btn_field3_clicked();
+
+    void on_btn_field4_clicked();
+
+    void on_btn_field5_clicked();
+
+    void on_btn_field6_clicked();
+
+    void on_btn_field7_clicked();
+
+    void on_btn_field8_clicked();
+
+    void on_btn_field9_clicked();
+
 private:
     Ui::JogoDaVelha *ui;
+    int AIScore = 0;
+    int userScore = 0;
+    QVector<int> fieldsSelected;
 };
 #endif // JOGODAVELHA_H
